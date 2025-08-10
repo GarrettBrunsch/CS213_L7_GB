6bb47d8b87f915190f30b05c2d417164516015aa
GarrettBrunsch <brunscgv1595@student.laccd.edu>
2025-08-10 02:09:11 -0700

    Update InvalidCreatureParamException.java

--- src/InvalidCreatureParamException.java
+++ src/InvalidCreatureParamException.java
@@ -1,7 +1,12 @@
+// Garrett Brunsch
+// Lab #7 - Abstract & Interface
+// Due 8/4/25
+
 class InvalidCreatureParamException extends RuntimeException
 {
     public InvalidCreatureParamException(String message)
     {
         super(message);
     }
+
 }

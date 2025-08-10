daabdc56ffdc88eb5a21d9ed823e49535a60b054
GarrettBrunsch <brunscgv1595@student.laccd.edu>
2025-08-10 02:09:02 -0700

    Update InvalidArmyParamException.java

--- src/InvalidArmyParamException.java
+++ src/InvalidArmyParamException.java
@@ -1,7 +1,12 @@
+// Garrett Brunsch
+// Lab #7 - Abstract & Interface
+// Due 8/4/25
+
 class InvalidArmyParamException extends RuntimeException
 {
     public InvalidArmyParamException(String message)
     {
         super(message);
     }
+
 }
